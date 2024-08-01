d52fd6a5e9e75bd88fc915326c1ca0e059134c4b
Gabriel Grabski <gabrielgrabski001@gmail.com>
2024-08-01 19:29:07 -0300

    Initiate Project

---
+++ src/main/java/com/grabas/gstore_auth_api/GstoreAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.grabas.gstore_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstoreAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstoreAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grabas/gstore_auth_api/GstoreAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabas.gstore_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstoreAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
